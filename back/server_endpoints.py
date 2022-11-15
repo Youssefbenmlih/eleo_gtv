07b8ad9edaff1aa5621995560b1c87a8608eedc3
YoussefB <youssefbenmlih@gmail.com>
2022-11-15 10:32:28 +0100

    only need inventaire, stock, error handling in querys

--- back/server_endpoints.py
+++ back/server_endpoints.py
@@ -1,56 +1,18 @@
-import pandas as pd
-from flask import Flask, request
-from flask import jsonify
-
-from db_connection import con, engine
+from flask import Flask
 
 app = Flask(__name__)
 
 from activity.demontage.dem_endpoints import demontage
 from activity.reception.rec_endpoints import reception
 from activity.chargement.cha_endpoints import chargement
+from user.user_endpoints import user
+app.register_blueprint(user)
 app.register_blueprint(demontage)
 app.register_blueprint(reception)
 app.register_blueprint(chargement)
 
 @app.route('/', methods=['GET'])
-def ls():
+def home():
     return "welcome to the eleo_gtv api"
-
-
-@app.route('/api/users/list', methods=['GET'])
-def get_users():
-    req = 'SELECT * FROM dbo.users'
-    return jsonify(pd.read_sql_query(req, engine).to_dict('records'))
-
-@app.route('/api/users/add', methods=['POST'])
-def add_user():
-    request_data = request.get_json()
-    req = '''INSERT INTO [dbo].[users]
-           ([name]
-           ,[email]
-           ,[mot_de_passe]
-           ,[admin])
-     VALUES
-           ('{0}', '{1}', '{2}', '{3}')'''.format(request_data['name'], request_data['email'], 
-                                                 request_data['mot_de_passe'], request_data['admin'])
-    con.execute(req)
-    return "200"
-    
-
-def check_connection(data):
-    req = 'SELECT name, email, mot_de_passe FROM dbo.users WHERE email = \'{}\''.format(data['email'])
-    users = pd.read_sql_query(req, engine).to_dict('records')
-    if len(users) == 1 and users[0]['mot_de_passe'] == data['mot_de_passe']:
-        return users[0]['name'], True
-    return "error", False
-
-@app.route('/api/users/connect', methods=['POST'])
-def connect_req():
-    request_data = request.get_json()
-    res = check_connection(request_data)
-    if (res[1]):
-        return res[0], "200"
-    return "403"
 
 app.run()
---
+++ back/user/user_endpoints.py
@@ -0,0 +1,50 @@
+from flask import request, Blueprint
+import pandas as pd
+from flask import jsonify
+
+from user.ac_user import *
+from db_connection import con, engine
+
+user = Blueprint('user', __name__, template_folder='templates')
+
+
+@user.route('/api/users/list', methods=['GET'])
+def get_users():
+    req = 'SELECT * FROM dbo.users'
+    return jsonify(pd.read_sql_query(req, engine).to_dict('records'))
+
+"""
+Json Content:
+{
+  "name": mr.name,
+  "email": "user@email.com", 
+  "mot_de_passe" : "mdp",
+  "admin" : "n"
+}
+"""
+@user.route('/api/users/add', methods=['POST'])
+def add_user():
+    request_data = request.get_json()
+    query = get_insert_query(request_data)
+    try:
+        with con.begin():
+            con.execute(query)
+    except Exception as e:
+        print(e)
+        return "400"
+    
+    return "200"
+
+"""
+{
+  "email": "user@email.com", 
+  "mot_de_passe" : "mdp"
+}
+"""
+@user.route('/api/users/connect', methods=['POST'])
+def connect_req():
+    request_data = request.get_json()
+    res = check_connection(request_data, engine)
+    if (res[1]):
+        return res[0], "200"
+    return "403"
